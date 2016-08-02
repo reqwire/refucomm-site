152a42bfa143c2094fe801a08dd029e109f2bfeb
Thomas Harvey <tom@alush.co.uk>
2016-08-02 19:19:31 +0100

    spelling

--- js/document-analytics.js
+++ js/document-analytics.js
@@ -4,8 +4,8 @@ Refucomm.record_document_get = function() {
     var href = $(this).attr('href'),
         document_name = $(this).data('documentname'),
         language = $(this).data('langs');
-    ga("send", "event", "inforpack", "download", document_name, language);
-    ga("send", "event", "inforpack", "translate", language);
+    ga("send", "event", "infopack", "download", document_name, language);
+    ga("send", "event", "infopack", "translate", language);
 };
 
 $(document).ready(function() {
