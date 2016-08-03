61086ba3eb328609ef7aba4731b645fef1b6463b
Thomas Harvey <tom@alush.co.uk>
2016-08-03 10:53:38 +0100

    adding link tracking

--- js/document-analytics.js
+++ js/document-analytics.js
@@ -8,6 +8,12 @@ Refucomm.record_document_get = function() {
     ga("send", "event", "infopack", "translate", language);
 };
 
+Refucomm.record_link_click = function() {
+    var link_name = $(this).data('linkname');
+    ga("send", "event", "infopack", "link", link_name);
+};
+
 $(document).ready(function() {
-  $('.document_list a').click( Refucomm.record_document_get );
+  $('.document_list .language_list a').click( Refucomm.record_document_get );
+  $('.link_list_item a').click( Refucomm.record_link_click );
 });
